a8258dcd644a20937faf265d2e8b10595cb32e8e
geraldgsh <geraldgsh@hotmail.com>
2020-04-03 15:08:28 +0800

    fixed lint error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './index.css';
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
