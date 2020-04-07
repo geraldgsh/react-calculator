781b15a02bd9275b5f413be1fedd03f7b2dcd848
geraldgsh <geraldgsh@hotmail.com>
2020-04-07 21:37:41 +0800

    added styling conditions

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 
 const Button = props => {
   const { name, color, half } = props;
-
+  const styling = {
+    width: half ? '50%' : '25%',
+    background: color
+  }
   return (
     <button className="buttons" type="button" style={styling}>
       {name}
