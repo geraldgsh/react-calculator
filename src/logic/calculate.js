46c4bf976adb297832458116076898980f62d676
geraldgsh <geraldgsh@hotmail.com>
2020-04-08 12:55:12 +0800

    added clear and negate in logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,15 @@
-const calculate = ({ total, next, operation }, buttonName) => {
+const calculate = (props, buttonName) => {
+  let { total, next, operation } = props;
+  if (buttonName === '+/-') {
+    if (total) total *= -1;
+    if (next) next *= -1;
+  }
+  if (buttonName === 'A/C') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+  return { total, next, operation };
+};
 
-}
+export default calculate;
