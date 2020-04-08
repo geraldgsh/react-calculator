4fcee80ad555c9561b88e8859ba23af9779e4d43
geraldgsh <geraldgsh@hotmail.com>
2020-04-09 07:09:48 +0800

    added more operands in operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,7 +2,17 @@ import Big from 'big-js';
 
 const operate = (numberOne, numberTwo, operation) => {
   const [one, two] = [Big(numberOne), Big(numberTwo)];
-  
+  switch (operation) {
+    case '+':
+      return one.plus(two);
+    case '-':
+      return one.minus(two);
+    case 'x':
+      return one.times(two);
+    case '/':
+      return one.div(two);
+  }
+
 }
 
 export default operate;
