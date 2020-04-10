9aa4156c7b3a0705af13432c97a254a0301fbc5b
geraldgsh <geraldgsh@hotmail.com>
2020-04-11 06:40:36 +0800

    corrected additional lint errors

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 import '../styles/styles.css';
 
 export default class Application extends React.Component {
