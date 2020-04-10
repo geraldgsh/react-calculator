9a7b7a1ca667c6fca0a8ff300dd20b74e945580f
geraldgsh <geraldgsh@hotmail.com>
2020-04-11 06:14:49 +0800

    added decimal conditions

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,17 +3,17 @@ import operate from './operate';
 const calculate = (props, buttonName) => {
   let { total, next, operation } = props;
   const operands = ['+', 'X', '-', '÷'];
-  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   if (buttonName === 'AC') {
     [total, next, operation] = [null, null, null];
   }
   if (buttonName === '+/-') {
-    if (total) (total *= -1).toString();
-    if (next) (next *= -1).toString();
+    if (total) (total *= -1);
+    if (next) (next *= -1);
     operation = null;
   }
   if (buttonName === '%') {
-    if (total) next = (0.01 * total).toString();
+    if (total) next = (0.01 * total);
   }
   if (buttonName === '=') {
     if (total && next && operation) {
@@ -23,11 +23,15 @@ const calculate = (props, buttonName) => {
     }
   }
   if (operands.includes(buttonName)) {
-    operation = buttonName;
-  } else if (operation && numbers.includes(buttonName)) {
+    if (total) operation = buttonName;
+  } else if (operation && nums.includes(buttonName)) {
     next = next ? next + buttonName : buttonName;
-  } else if (numbers.includes(buttonName)) {
+  } else if (nums.includes(buttonName)) {
     total = total ? total + buttonName : buttonName;
+  } else if (!next && !operation && buttonName ==='.') {
+    total = total.includes(buttonName) ? total : total + buttonName;
+  } else if (total && operation && buttonName ==='.') {
+    next = next.includes(buttonName) ? next : next + buttonName;
   }
   return { total, next, operation };
 };
